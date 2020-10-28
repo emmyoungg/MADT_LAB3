50e4f1ffcefc1cc65bf0e8dc71eba3bddf386d41
emmyoungg <56218355+emmyoungg@users.noreply.github.com>
2020-10-28 18:52:50 +0200

    inital commmit

---
+++ app/src/main/java/com/example/madt_lab4/MainActivity.java
@@ -0,0 +1,251 @@
+package com.example.madt_lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    double in1 = 0, i2 = 0;
+    TextView edittext1;
+    boolean Add, Sub, Multiply, Divide, Remainder, deci;
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnAddition, btnSubtraction,
+            btnMultiply, btnDivision, btnEquals, btnDelete, btnPeriod, btnClear, btnClearEverything, btnSquareRoot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn0 = (Button) findViewById(R.id.btn0);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);
+        btnAddition = (Button) findViewById(R.id.btnAddition);
+        btnSubtraction = (Button) findViewById(R.id.btnSubtract);
+        btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        btnDivision = (Button) findViewById(R.id.btnDivision);
+        btnPeriod = (Button) findViewById(R.id.btnPeriod);
+        btnEquals = (Button) findViewById(R.id.btnEquals);
+        btnDelete = (Button) findViewById(R.id.btnClearEverything);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnClearEverything = (Button) findViewById(R.id.btnClearEverything);
+        btnSquareRoot = (Button) findViewById(R.id.btnSquareRoot);
+
+        edittext1 = (TextView) findViewById(R.id.result_id);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "1");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "2");
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "3");
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "4");
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "5");
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "6");
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "7");
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "8");
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "9");
+            }
+        });
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "0");
+            }
+        });
+
+        btnAddition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edittext1.getText().length() != 0) {
+                    in1 = Float.parseFloat(edittext1.getText() + "");
+                    Add = true;
+                    deci = false;
+                    edittext1.setText(null);
+                }
+            }
+        });
+
+        btnSubtraction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edittext1.getText().length() != 0) {
+                    in1 = Float.parseFloat(edittext1.getText() + "");
+                    Sub = true;
+                    deci = false;
+                    edittext1.setText(null);
+                }
+            }
+        });
+
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edittext1.getText().length() != 0) {
+                    in1 = Float.parseFloat(edittext1.getText() + "");
+                    Multiply = true;
+                    deci = false;
+                    edittext1.setText(null);
+                }
+            }
+        });
+
+        btnDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edittext1.getText().length() != 0) {
+                    in1 = Float.parseFloat(edittext1.getText() + "");
+                    Divide = true;
+                    deci = false;
+                    edittext1.setText(null);
+                }
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edittext1.getText().length() != 0) {
+                    in1 = Float.parseFloat(edittext1.getText() + "");
+                    Remainder = true;
+                    deci = false;
+                    edittext1.setText(null);
+                }
+            }
+        });
+
+        btnEquals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Add || Sub || Multiply || Divide || Remainder) {
+                    i2 = Float.parseFloat(edittext1.getText() + "");
+                }
+
+                if (Add) {
+
+                    edittext1.setText(in1 + i2 + "");
+                    Add = false;
+                }
+
+                if (Sub) {
+
+                    edittext1.setText(in1 - i2 + "");
+                    Sub = false;
+                }
+
+                if (Multiply) {
+                    edittext1.setText(in1 * i2 + "");
+                    Multiply = false;
+                }
+
+                if (Divide) {
+                    edittext1.setText(in1 / i2 + "");
+                    Divide = false;
+                }
+                if (Remainder) {
+                    edittext1.setText(in1 % i2 + "");
+                    Remainder = false;
+                }
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText("");
+                in1 = 0.0;
+                i2 = 0.0;
+            }
+        });
+
+        btnPeriod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (deci) {
+                    //do nothing or you can show the error
+                } else {
+                    edittext1.setText(edittext1.getText() + ".");
+                    deci = true;
+                }
+
+            }
+        });
+
+        btnClearEverything.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText("");
+                in1 = 0.0;
+                i2 = 0.0;
+            }
+        });
+
+        btnSquareRoot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText("");
+                in1 = 0.0;
+                i2 = 0.0;
+            }
+        });
+    }
+}
